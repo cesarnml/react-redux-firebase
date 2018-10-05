4e1348b712d13a7f50f8d27e20f480b001e78d5a
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2018-10-04 22:49:46 -0700

    lesson 9

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { Switch, Route } from 'react-router-dom'
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails'
 import SignIn from './components/auth/SignIn'
-
+import SignUp from './components/auth/SignUp'
+import CreateProject from './components/projects/CreateProject'
 class App extends Component {
   render () {
     return (
@@ -14,6 +15,9 @@ class App extends Component {
           <Route exact path='/' component={Dashboard} />
           <Route path='/project/:id' component={ProjectDetails} />
           <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
+          <Route path='/create' component={CreateProject} />
+
         </Switch>
       </div>
     )
--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -29,6 +29,9 @@ class SignIn extends Component {
             <label htmlFor='password'>Password</label>
             <input type='password' id='password' onChange={this.handleChange} />
           </div>
+          <div className='input-field'>
+            <button className='btn pink lighten-1 z-depth-0'>Login</button>
+          </div>
         </form>
       </div>
     )
